900c7bfc877e5a38e793a3a057286827bfbc1907
Dagrate123 <sigia002@osloskolen.no>
2026-05-07 09:03:13 +0200

    fixed movement

--- client.py
+++ client.py
@@ -14,35 +14,26 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         # Create a 50x50 green square
-        self.image = pygame.Surface((50, 50))
+        self.image = pygame.Surface((20, 20))
         self.image.fill((0, 255, 0)) 
         
         # Set initial position
         self.rect = self.image.get_rect()
         self.rect.center = (screen_width // 2, screen_height // 2)
-        self.speed = 2
+        self.speed = 5
 
     def update(self, keys):
-        directions = {
-            "left": False,
-            "right": False,
-            "up": False,
-            "down": False
-        }
-        # Movement logic
-        if keys[pygame.K_LEFT] and self.rect.left > 0 and list(directions.values()).count(False) == 3:
+        if keys[pygame.K_LEFT] and self.rect.left > 0:
             self.rect.x -= self.speed
-            directions["left"] = True
-        if keys[pygame.K_RIGHT] and self.rect.right < screen_width and list(directions.values()).count(False) == 3:
-            self.rect.x += self.speed
-            directions["right"] = True
-        if keys[pygame.K_UP] and self.rect.top > 0 and list(directions.values()).count(False) == 3:
-            self.rect.y -= self.speed
-            directions["up"] = True
-        if keys[pygame.K_DOWN] and self.rect.bottom < screen_height and list(directions.values()).count(False) == 3:
-            self.rect.y += self.speed
-            directions["down"] = True
 
+        elif keys[pygame.K_RIGHT] and self.rect.right < screen_width:
+            self.rect.x += self.speed
+            
+        elif keys[pygame.K_UP] and self.rect.top > 0:
+            self.rect.y -= self.speed
+
+        elif keys[pygame.K_DOWN] and self.rect.bottom < screen_height:
+            self.rect.y += self.speed
 # 4. Setup Game Objects
 player = Player()
 all_sprites = pygame.sprite.Group()
